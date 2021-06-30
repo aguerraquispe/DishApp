3b1d449408b76f034042440396a2c3e1ca359ceb
aguerraquispe <aguerraquispe@users.noreply.github.com>
2021-06-30 03:16:36 -0500

    Activitys de usuario agregados

---
+++ app/src/main/java/com/example/dishapp/user_info_plato.java
@@ -0,0 +1,14 @@
+package com.example.dishapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class user_info_plato extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info_plato);
+    }
+}
