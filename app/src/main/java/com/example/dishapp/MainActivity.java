7121567b4a79cde0c39b0f4dd5a3d2da1590502c
aguerraquispe <aguerraquispe@users.noreply.github.com>
2021-07-04 21:03:31 -0500

    boton retroceso y categorias mostradas

--- app/src/main/java/com/example/dishapp/MainActivity.java
+++ app/src/main/java/com/example/dishapp/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton btnAdmin;
+    String categoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +20,46 @@ public class MainActivity extends AppCompatActivity {
 
         btnAdmin = (ImageButton) findViewById(R.id.buttonAdmin);
 
+
         btnAdmin.setOnClickListener(view -> {
 
             Intent intent = new Intent(this, AdminLogin.class);
             startActivity(intent);
         });
     }
+
+    public void VerPlatosALaCarta(View view){
+        categoria = "Platos a la carta";
+        Toast.makeText(this,"Ingresaste a Platos a la carta",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,User_lista_platos.class);
+        intent.putExtra("categoria",categoria);
+        startActivity(intent);
+        finish();
+    }
+
+    public void VerBebidas(View view){
+        categoria = "Bebidas";
+        Toast.makeText(this,"Ingresaste a Bebidas",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,User_lista_platos.class);
+        intent.putExtra("categoria",categoria);
+        startActivity(intent);
+        finish();
+    }
+    public void VerRapido(View view){
+        categoria = "Fast Food";
+        Toast.makeText(this,"Ingresaste a Fast Food",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,User_lista_platos.class);
+        intent.putExtra("categoria",categoria);
+        startActivity(intent);
+        finish();
+    }
+    public void VerPostres(View view){
+        categoria = "Postres";
+        Toast.makeText(this,"Ingresaste a Postres",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,User_lista_platos.class);
+        intent.putExtra("categoria",categoria);
+        startActivity(intent);
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/dishapp/User_lista_platos.java
+++ app/src/main/java/com/example/dishapp/User_lista_platos.java
@@ -2,13 +2,34 @@ package com.example.dishapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class User_lista_platos extends AppCompatActivity {
+    String categoria;
+    private TextView lblCategoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_lista_platos);
+
+        //asignando valores
+        lblCategoria =(TextView) findViewById(R.id.lblCategoria);
+
+        //seteando de activity anterior
+        categoria = getIntent().getStringExtra("categoria");
+        lblCategoria.setText(categoria);
+
+    }
+
+    public void RegresarAMain(View view){
+        Toast.makeText(this,"Regresaste a Main",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
